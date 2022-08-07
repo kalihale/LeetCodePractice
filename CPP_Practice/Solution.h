e620cc8fb81dff07acfa2d9d062b95277b39a5ab
Kali <kalihale@outlook.com>
2022-08-06 23:42:38 -0700

    finished containsDuplicate

--- CPP_Practice/Solution.h
+++ CPP_Practice/Solution.h
@@ -4,21 +4,19 @@
 
 #ifndef LEETCODEPRACTICE_SOLUTION_H
 #define LEETCODEPRACTICE_SOLUTION_H
+
 #include <vector>
+#include <map>
 
 
-class Solution
-{
+class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target)
-    {
+    // ／(^ㅅ^)＼ O(n^2) solution
+    std::vector<int> twoSum(std::vector<int> &nums, int target) {
         std::vector<int> solution;
-        for(int i = 0; i < nums.size() - 1; i++)
-        {
-            for(int j = i + 1; j < nums.size(); j++)
-            {
-                if(nums[i] + nums[j] == target)
-                {
+        for(int i = 0; i < nums.size() - 1; i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if(nums[i] + nums[j] == target) {
                     solution.push_back(i);
                     solution.push_back(j);
                     return solution;
@@ -26,6 +24,40 @@ public:
             }
         }
         return solution;
+    }
+
+    int removeDuplicates(std::vector<int> &nums) {
+        // ／(^ㅅ^)＼ k is the length of the final vector
+        //           k will also be used as the "anchor point" for where to compare numbers
+        int k = 1;
+        // ／(^ㅅ^)＼ Start loop at k, which will be one greater than the number to compare to
+        for(int i = k; i < nums.size(); ++i) {
+            // ／(^ㅅ^)＼ Find the next larger number than the number at the "anchor point"
+            if(nums[k - 1] < nums[i]) {
+                // ／(^ㅅ^)＼ Set the next number to the larger number and increment k
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    bool containsDuplicate(std::vector<int> &nums) {
+        std::map<int, int> map;
+        for(int i: nums) {
+            // ／(^ㅅ^)＼ If the element doesn't exist, add the element
+            //           map efficiency is O(log n)
+            if(map.find(i) == map.end())
+            {
+                map[i] = 1;
+            }
+            // ／(^ㅅ^)＼ Otherwise, there is a duplicate
+            else
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
